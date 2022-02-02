794a091e861a5e978bcb91384f426d0cae156e84
jehyeon <hyeon.rot@gmail.com>
2022-02-03 00:23:27 +0900

    add enemy attack;

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,21 +4,87 @@ using UnityEngine;
 
 public class Enemy : Character
 {
-    // Start is called before the first frame update
+    public GameObject player;
+    private Animator animator;
+
+    [SerializeField]
+    private GameObject go_enemyWeapon;   // 직접 넣어줘야 함
+    private BoxCollider enemyWeaponCollider;
+
+    private bool isMove;
+    private bool canAttack;     // 공격 범위 안에 있는지
+    private float attackRange = 2f;    // temp
+
     void Start()
     {
-        
+        player = GameObject.Find("Player");
+        // Enemy table 만들기 전까지 임시
+        _stat.Speed = 5;
+        _stat.Damage = 5;
+        _stat.Hp = 10;
+
+        animator = GetComponent<Animator>();
+        enemyWeaponCollider = go_enemyWeapon.GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
     protected override void Update()
     {
         base.Update();
+
+        Act();
     }
 
+    private void Act()
+    {
+        ActivateWeapon();
+
+        Vector3 dir = player.transform.position - this.transform.position;
+        if (dir.magnitude < attackRange)
+        {
+            isMove = false;
+            animator.SetBool("isMove", false);
+            animator.SetTrigger("isAttack");
+        }
+        else
+        {
+            isMove = true;
+            animator.SetBool("isMove", true);
+            this.transform.position += dir.normalized * Time.deltaTime * _stat.Speed;
+            // 지상 유닛은 x 축이 회전되면 안됨
+            dir.y = 0f;
+            this.transform.rotation = Quaternion.LookRotation(dir.normalized);
+        }
+    }
+
+    // 무기 collider 활성화
+    private void ActivateWeapon()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(1).IsName("Attack"))
+        {
+            enemyWeaponCollider.enabled = true;
+        }
+        else
+        {
+            enemyWeaponCollider.enabled = false;
+        }
+    }
+
+    // 공격
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<Player>().GetDamage(_stat.Damage);
+        }
+
+        return;
+    }
+
+    // 피격
     public void GetDamage(int damage)
     {
         _stat.DecreaseHp(damage);
         Debug.Log(_stat.Hp);
-    }    
+    }
 }
